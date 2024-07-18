b356c93c956e4b7f17c3dca69e6dc1bc72a30cd5
Pedro Cardoso <peedro.shell@gmail.com>
2024-07-18 15:50:50 -0300

    config font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={inter.className}>
       <body>{children}</body>
     </html>
   )
