92dcb06b5e60fb1c73f53867438dc6056fbfb232
Pedro Cardoso <peedro.shell@gmail.com>
2024-07-20 16:07:52 -0300

    update file path

--- src/scripts/cache.ts
+++ src/scripts/cache.ts
@@ -1,12 +1,12 @@
 import fs from 'fs';
 import grayMatter from 'gray-matter';
+import {join} from 'path';
 import { remark } from 'remark';
 import remarkHTML from 'remark-html';
-
 export function loadPosts() {
   const files = fs.readdirSync('_posts');
   const posts = files.map(file => {
-    const fileContent = fs.readFileSync(`_posts/${file}`, 'utf8');
+    const fileContent = fs.readFileSync(join(__dirname, '..', '..', '..', '_posts', file), 'utf-8');
     const { data } = grayMatter(fileContent);
     const { title, date } = data;
 
